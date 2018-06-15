78d145f513bc090254d346e323ab54470dddd537
wjk <mywjkang@163.com>
2018-06-15 17:40:16 +0800

    文章页面添加

--- application/index/controller/Article.php
+++ application/index/controller/Article.php
@@ -5,6 +5,7 @@
 // | Time: 2018/6/5 9:07
 // +----------------------------------------------------------------------
 namespace app\index\controller;
+use think\Model;
 class Article extends Base
 {
 	/** 
@@ -12,7 +13,14 @@ class Article extends Base
 	* @access public 
 	*/ 
 	public function index()
-	{
+	{	
+		//数据渲染
+		$this->assign('article',model('Article')->getArticle());
+		$this->assign('articleClickList',model('Article')->getArticleClickList());
+		$this->assign('articleRecommend',model('Article')->getArticleRecommend());
+		//文章类型数据获取
+		$articleTypeData = db('article_type')->where(['is_show'=>1])->order(['add_time'=>'desc'])->select();
+		$this->assign('articleType',$articleTypeData);
 		return view('index');
 	}
 

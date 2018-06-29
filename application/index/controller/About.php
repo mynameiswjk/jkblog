e60a202fa9d137bcd3180bd95dfde958a3b2567b
wjk <mywjkang@163.com>
2018-06-29 13:21:02 +0800

    前台友情链接信息展示

--- application/index/controller/About.php
+++ application/index/controller/About.php
@@ -8,8 +8,22 @@ namespace app\index\controller;
 class About extends Base
 {
 	public function index()
-	{
+	{	
+		//关于博客
+
+		//关于作者
+
+		//友情链接
+		$this->getBlogroll();
+		//留言墙
 		return view('index');
 	}
 
+	//获得友情链接数据
+	public function getBlogroll()
+	{
+		$blogrollData = db('blogroll')->where(['is_show'=>1])->order(['blogroll_addtime'=>'desc'])->select();
+		//页面数据分配
+		$this->assign('blogroll',$blogrollData);
+	}
 }

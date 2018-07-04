c53251e5266b4afcffe0e4ce51880f0793f80243
wjk <mywjkang@163.com>
2018-07-04 16:07:44 +0800

    修改后台网站设置模块

--- application/index/controller/Base.php
+++ application/index/controller/Base.php
@@ -7,6 +7,7 @@
 namespace app\index\controller;
 use think\Controller;
 use think\Session;
+use think\Cache;
 /** 
 * 前台控制器基类
 * @access public 
@@ -18,6 +19,12 @@ class Base extends Controller
 		parent::__construct();
 		//如果登录信息渲染
 		if($userInfo = session('userInfo')) $this->assign('userInfo',$userInfo);
+		//获得网站配置信息
+		$website_setting = Cache::get('website_setting');
+		if(empty($website_setting)) $website_setting = db('site')->find();
+		if($website_setting['website_status'] == 0) die($website_setting['website_close_cause']);
+		$this->assign('website_setting',$website_setting);
+
 	}
 	//退出登录
 	function logout()

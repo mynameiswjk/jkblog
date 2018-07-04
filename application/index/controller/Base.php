87c7c1042c649a7496114618dfc9b20f50abe3bf
wjk <mywjkang@163.com>
2018-07-04 17:38:11 +0800

    修改公共头部seo_title

--- application/index/controller/Base.php
+++ application/index/controller/Base.php
@@ -24,11 +24,42 @@ class Base extends Controller
 		if(empty($website_setting)) $website_setting = db('site')->find();
 		if($website_setting['website_status'] == 0) die($website_setting['website_close_cause']);
 		$this->assign('website_setting',$website_setting);
+		//获得seo标题
+		$this->getSeoTitle();
+
 	}
 	//退出登录
 	function logout()
 	{
 		Session::delete('userInfo');
 		$this->success('您已退出登录','Login/index');
+	}
+
+	//seo标题
+	private function getSeoTitle()
+	{
+		$controller = request()->controller();
+		switch ($controller) {
+			case 'Article':
+				$seo_title = '|文章专栏';
+				break;
+			case 'Mixed':
+				$seo_title = '|照片专栏';
+				break;
+			case 'Timeline':
+				$seo_title = '|时光轴';
+				break;
+			case 'About':
+				$seo_title = '|关于我们';
+				break;
+			case 'Member':
+				$seo_title = '|用户中心';
+				break;
+			case 'Login':
+				$seo_title = '|登录-注册模块';
+				break;
+			default : $seo_title = '';
+		}
+		$this->assign('seo_title',$seo_title);	
 	}
 }

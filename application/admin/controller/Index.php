a015f8c2f46ea958f374084f9370b1e3dab1526f
wjk <mywjkang@163.com>
2018-07-05 15:51:59 +0800

    ‘后台首页一些数据的线上

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -26,7 +26,21 @@ class Index extends Base
 	public function main()
 	{	
 		$VersionsInfo = getVersionsInfo();
-		
+		//留言总数
+		$this->assign('messageCount',db('message')->count());
+		//新增用户，今天添加的用户
+		//获得今天0.00分的时间
+		$toDay = strtotime(date('Y-m-d',time()));
+		//找出大于今天0点数据
+		$this->assign('newUserCount',db('user')->where(['user_addtime'=>['lt','user_addtime']])->count());
+		//用户总数
+		$this->assign('memberCount',db('user')->count());
+		//图片总数
+		$this->assign('photoCount',db('photo')->count());
+		//文章总数
+		$this->assign('articleCount',db('article')->count());
+		//获取最新的7条文章数据
+		$this->assign ('articleData',db('article')->limit(7)->order(['article_addtime'=>'desc'])->select());
 		return view('main',['data'=>$VersionsInfo]);
 	}
 }

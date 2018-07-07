1861cd4677ad0a26d510fc32329e40cd9231de3c
wjk <mywjkang@163.com>
2018-07-07 14:12:27 +0800

    修改文件上传JS

--- public/static/admin/js/images_add.js
+++ public/static/admin/js/images_add.js
@@ -4,12 +4,17 @@ layui.use(['upload','form'], function(){
   ,upload = layui.upload;
 
   //多图片上传
+   var up;
   upload.render({
     elem: '#test2'
     ,url:uploadPhotoUrl
     ,multiple: true
+    ,before:function (res){
+         up = layer.msg('数据提交中，请稍候',{icon: 16,time:false,shade:0.8});
+    }
     ,done: function(res){
       //上传完毕
+        layer.close(up);
         $('#demo2').append('<div style="display:inline-block;margin-right:20px;"><img  style="max-width:300px;height:150px;" src="'+ res.file_url +'" alt="" class="layui-upload-img"><input type="hidden" name="photo_thumb" value="'+res.file_url+'"><div class="layui-form-item" style="margin-top:10px;"><div class="layui-input-inline"><input type="text" name="photo_name"  id="photo_name" placeholder="请填写图片名称" autocomplete="off" class="layui-input"> </div></div></div>');
     }
   });

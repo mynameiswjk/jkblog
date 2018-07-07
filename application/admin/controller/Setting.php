d75dbad197dad2cc358e2dc15081dc6edfa90289
root <mywjkang@163.com>
2018-07-07 10:46:39 +0800

    修改后台网址设置

--- application/admin/controller/Setting.php
+++ application/admin/controller/Setting.php
@@ -27,25 +27,25 @@ class Setting extends Base
 	 		$data = input('post.');
 	 		//验证
 	 		if(empty($data['website_name'])) die(json_encode(['code'=>500,'msg'=>'请输入网站名称']));
-	 	}
-	 	//数据库操作
-	 	if(!$site_id = db('site')->value('site_id')) {
-	 		//数据第一次添加
-	 		if(db('site')->strict(false)-insert($data)) {
-					Cache::set('website_setting',$data);
-					die(json_encode(['code'=>200,'msg'=>'设置网站信息成功']));
-			}else{
-					die(json_encode(['code'=>500,'msg'=>'设置网站信息失败']));
-			}
-	 	}else{
-	 		//数据修改
-	 		if(db('site')->where(['site_id'=>$site_id])->strict(false)->update($data) !== FALSE) {
-	 			Cache::set('website_setting',$data);
-	 			die(json_encode(['code'=>200,'msg'=>'设置网站信息成功']));
-			}else{
+		 	//数据库操作
+		 	if(!$site_id = db('site')->value('site_id')) {
+		 		//数据第一次添加
+		 		if(db('site')->strict(false)->insert($data)) {
+						Cache::set('website_setting',$data);
+						die(json_encode(['code'=>200,'msg'=>'设置网站信息成功']));
+				}else{
+						die(json_encode(['code'=>500,'msg'=>'设置网站信息失败']));
+				}
+		 	}else{
+		 		//数据修改
+		 		if(db('site')->where(['site_id'=>$site_id])->strict(false)->update($data) !== FALSE) {
+		 			Cache::set('website_setting',$data);
+		 			die(json_encode(['code'=>200,'msg'=>'设置网站信息成功']));
+				}else{
 
-				die(json_encode(['code'=>500,'msg'=>'设置网站信息失败']));
-			}
+					die(json_encode(['code'=>500,'msg'=>'设置网站信息失败']));
+				}
+		 	}
 	 	}
 	 }
 }
